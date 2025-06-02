29e6dc8ca734c89af1d89fb2f8dbb3c9f7a0bc9d
meet-vasita <meetvasita85@gmail.com>
2025-06-02 15:16:49 +0530

    Done

---
+++ Programs/Score_Calculator.py
@@ -0,0 +1,19 @@
+# There was a test in your class and you passed it. Congratulations!
+
+# But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+# You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+# Return true if you're better, else false!
+# Note:
+
+# Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+def better_than_average(class_points):
+    for i in class_points:
+        print(i)
+
+
+arr = better_than_average([1,2,3,4,5])
+
+print(arr)
--- Programs/dna_rna_conversion.py
+++ Programs/dna_rna_conversion.py
@@ -10,3 +10,7 @@
 
 # The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
+def dna_to_rna(dna):
+    return dna.replace("T", "U")
+
+print(dna_to_rna('GCAT'))
